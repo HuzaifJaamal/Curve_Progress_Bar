3629951b31cfa3cd3cf096b7d9d6eed424e96fdd
HuzaifJaamal <huzaifajamal95@gmail.com>
2023-08-24 11:20:51 +0500

    Quadratic Bezier Curve

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:mywave/mywave.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MyWaveClipper(),
+//     );
+//   }
+// }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWaveClipper(),
+    );
+  }
+}
